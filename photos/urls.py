4ed4c3050baa408e7827e8d2708fb5f6f526f9f8
Saitore973 <saitorevg@gmail.com>
2022-03-25 14:10:21 +0300

    create a past date view function

--- photos/urls.py
+++ photos/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
-    path('current/',views.photos_of_day,name = 'photosToday')
-]
+    path('current/',views.photos_of_day,name = 'photosToday'),
+    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
+    ] 
