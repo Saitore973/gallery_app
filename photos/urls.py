69f034389bfd8ed33f442c7b5e1cb627a68fa5b5
Saitore973 <saitorevg@gmail.com>
2022-03-26 14:21:49 +0300

    creation of media folder after uploading the first image

--- photos/urls.py
+++ photos/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, re_path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
@@ -8,3 +10,5 @@ urlpatterns=[
     path('search/', views.search_results, name='search_results')
     ] 
 
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
