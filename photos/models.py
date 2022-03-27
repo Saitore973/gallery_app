a8bc4e17012201252f29791f1d4826ea35618562
Saitore973 <saitorevg@gmail.com>
2022-03-27 23:51:11 +0300

    add methods for location and category models

--- photos/models.py
+++ photos/models.py
@@ -8,9 +8,14 @@ class Location(models.Model):
     location_contact = models.CharField(max_length = 10,blank =True)
 
     def __str__(self):
-        
+        return self.location_name
 
-            return self.location_name
+    def save_location(self):
+            self.save()
+
+    def delete_location(self):
+        self.delete()
+
 
     class Meta:
             ordering = ['name']
@@ -20,6 +25,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()    
 
 class Image(models.Model):
     image_name = models.CharField(max_length =30)
@@ -50,3 +61,24 @@ class Image(models.Model):
     def filter_by_location(cls, location):
         images = cls.objects.filter(location__name__icontains=location)
         return images
+
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    class Meta:
+        ordering = ['date']
